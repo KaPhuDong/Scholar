3605ef86f9f901adfff35df68deaa64ea42760c9
viWhnga <vihang05092005@gmail.com>
2024-12-12 15:11:10 +0700

    build category

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -15,6 +15,23 @@
         </div>
         <img src="https://img.freepik.com/premium-photo/school-supplies-grey-background_78621-574.jpg?w=826" alt="" class="img-hero">  
     </div> 
+    <!-- Category -->
+    <div class="category-cards">
+        <a href="/Scholar/Writes" class="category-card">
+            <img src="./public/assets/images/category-1.jpg" alt="Writes" class="img-item">
+            <div class="text">Writes</div>
+        </a>
+        <a href="/Scholar/Notes" class="category-card">
+            <img src="./public/assets/images/category-2.jpg" alt="Notes" class="img-item">
+            <div class="text">Notes</div>
+        </a>
+        <a href="/Scholar/Gears" class="category-card">
+            <img src="./public/assets/images/category-3.jpg" alt="Gears" class="img-item">
+            <div class="text">Gears</div>
+        </a>
+    </div>
+
+
     <div class="cards extra-cards">
         <div class="card extra-card">
             <img src="https://www.shutterstock.com/image-vector/cute-cat-wear-dino-costume-600nw-2457633459.jpg" alt="product" class="img">
