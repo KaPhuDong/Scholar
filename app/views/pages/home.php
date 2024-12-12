0cb6284323ff6631858fe91e1dc54b1854272c42
Bang-05 <bang.zoan26@student.passerellesnumeriques.org>
2024-12-12 21:24:07 +0700

    add HTML structure for slider

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -52,6 +52,15 @@
             </div>
         </div>
     </div>
+<!-- Slider -->
+    <div class="home-slider">
+        <div class="slides">
+            <div class="slide"><img src="https://i.pinimg.com/736x/2c/37/c6/2c37c6df47e05a3c75e6e7d7fbfbafeb.jpg" alt="Slide 1"></div>
+            <div class="slide"><img src="https://i.pinimg.com/736x/2c/37/c6/2c37c6df47e05a3c75e6e7d7fbfbafeb.jpg" alt="Slide 2"></div>
+            <div class="slide"><img src="https://i.pinimg.com/736x/2c/37/c6/2c37c6df47e05a3c75e6e7d7fbfbafeb.jpg" alt="Slide 3"></div>
+            <div class="slide"><img src="https://i.pinimg.com/736x/2c/37/c6/2c37c6df47e05a3c75e6e7d7fbfbafeb.jpg" alt="Slide 1"></div>
+        </div>
+    </div>
 </body>
 
 </html>
