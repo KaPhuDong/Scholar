f1cca4240a73f73c0aca4f9bc07b3b1c6804c043
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2024-12-21 09:33:28 +0700

    fix css link

--- app/views/main.php
+++ app/views/main.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main</title>
     <base href="http://localhost:8080/Scholar/">
-    <link type="text/css" rel="stylesheet" href="public/styles/main.css">
+    <link rel="stylesheet" href="public/styles/main.css">
 </head>
 
 <body>
