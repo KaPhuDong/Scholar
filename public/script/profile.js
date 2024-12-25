8e6cfba3d573313bf83ebe07ac959a968890f047
Bang-05 <bang.zoan26@student.passerellesnumeriques.org>
2024-12-25 16:48:33 +0700

    78 add js for profile

---
+++ public/script/profile.js
@@ -0,0 +1,13 @@
+function Avater(event) { 
+    var reader = new FileReader(); 
+    reader.onload = function() { 
+        var output = document.querySelector('.avatar-preview');
+ 
+        output.src = reader.result; 
+    }; 
+    reader.readAsDataURL(event.target.files[0]); 
+}
+
+    document.querySelector('.upload-image').addEventListener('click', function() { 
+        document.querySelector('.update-image').click(); 
+    });
