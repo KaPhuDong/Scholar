579632e4982c365e5d5c3821fda1293dae547db1
Bang-05 <bang.zoan26@student.passerellesnumeriques.org>
2024-12-26 16:48:59 +0700

    78 update profile

--- app/views/user/pages/auth/profile.php
+++ app/views/user/pages/auth/profile.php
@@ -4,7 +4,7 @@ $userData = $data["userData"];
 <div class="container-profile">
     <div class="user-infor">
         <div class="box">
-            <img class="avatar-user" alt="Profile image" src="./public/assets/images/<?php echo $userData['avatar']; ?>">
+            <img class="avatar-user" alt="Profile image" src="/public/assets/images/<?php echo $userData["avatar"]?>">
             <div class="user-content">
                 <h2 class="user-name"><?php echo $userData['name']; ?></h2>
                 <p class="user-email"><?php echo $userData['email']; ?></p>
@@ -20,7 +20,7 @@ $userData = $data["userData"];
     </div>
 
     <div class="profile-page">
-        <form action="" method="POST" enctype="multipart/form-data" class="profile">
+        <form action="/Scholar/User/updateProfile" method="POST" enctype="multipart/form-data" class="profile">
 
             <div class="form-infor">
                 <h1 class="my-profile">My profile</h1>
@@ -48,10 +48,46 @@ $userData = $data["userData"];
             <div class="form-user-avatar">
                 <label class="upload-image" for="chooseImage">Choose image</label>
                 <img class="avatar-preview" alt="Profile image" src="/public/assets/images/<?php echo $userData["avatar"]?>">
-                <input id="choose-image" type="file" class="update-image" accept="image/*" onchange="Avatar(event)">
-                <button class="save-changes" name="update">Cancel</button>
+                <input id="choose-image" name="avatar" type="file" class="update-image" accept="image/*" onchange="Avatar(event)">
+                <button class="save-changes" type="button" name="update" onclick="cancelChanges()">Cancel</button>
+
             </div>
 
         </form>
     </div>
 </div>
+
+<script>
+    function Avatar(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = document.querySelector('.avatar-preview');
+        output.src = reader.result;
+
+        var userAvatar = document.querySelector('.avatar-user');
+        if (userAvatar) {
+            userAvatar.src = reader.result; 
+        }
+    };
+    reader.readAsDataURL(event.target.files[0]);
+    }
+
+    document.querySelector('.upload-image').addEventListener('click', function() {
+        document.querySelector('.update-image').click(); 
+    });
+
+    function cancelChanges() {
+
+    var originalAvatar = "/public/assets/images/<?php echo $userData["avatar"]?>"; 
+    document.querySelector('.avatar-preview').src = originalAvatar;  
+    document.querySelector('.avatar-user').src = originalAvatar;     
+
+    document.querySelector('input[name="username"]').value = "<?php echo $userData['name']; ?>";
+    document.querySelector('input[name="phonenumber"]').value = "<?php echo $userData['phone_number']; ?>";
+    document.querySelector('input[name="email"]').value = "<?php echo $userData['email']; ?>";
+    document.querySelector('input[name="address"]').value = "<?php echo $userData['address']; ?>";
+
+    document.querySelector('#choose-image').value = ''; 
+    }
+
+</script>
